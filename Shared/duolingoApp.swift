1b2afb6c592f0f7833562791afb09eee958591f0
Kanishk Vijaywargiya <kanishkvijaywargiya99@gmail.com>
2021-06-02 13:48:37 +0530

    Initial Commit

---
+++ Shared/duolingoApp.swift
@@ -0,0 +1,17 @@
+//
+//  duolingoApp.swift
+//  Shared
+//
+//  Created by KANISHK VIJAYWARGIYA on 02/06/21.
+//
+
+import SwiftUI
+
+@main
+struct duolingoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
